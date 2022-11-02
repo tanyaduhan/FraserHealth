3cd3fd1c4fb02714b230414c037274870502da11
Tanya Duhan <tanyaduhan@gmail.com>
2022-11-01 19:34:08 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+api_key = 'l16QJiC0CoLTgjlFX0DReLemz'
+api_key_secret = 'ZnmmgFt4FBld7CYm7f5qNoIcH3HU2wd0wRYJsIWao7sAsOHm4F'
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAALAIiAEAAAAA%2BU1AKIW2TxOfAL4qSHQRy8YGZAs%3DcGKKAq6dOKpPhrz5Yt2SmvY1ChTtslPgzwaaYzxlHaOtXy75m0'
+academic_bearer_token = 'AAAAAAAAAAAAAAAAAAAAALAIiAEAAAAAvQqL53X9Qk0v%2FCmfzu8mDvi5Snw%3D09kIuZvCyFgrBr3UGtq3jmruRbdL80LsqvBQvzcPA18RD9yjMt'
+access_token = '1579353986607779840-ihTcMElteDK9HogAGc9cP6JsrJiOFn'
+access_token_secret = '1h9Uq1gtFPqhqDvfHv4NTuP6nxJsP175okbNY0AC1MnMI'
